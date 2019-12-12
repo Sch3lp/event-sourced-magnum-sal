b0571b613f5af61812536b4aa9cba197b816bf57
sch3lp <sch3lp@gmail.com>
2019-12-12 13:05:50 +0100

    Failing test for placing a miner

--- src/main/kotlin/be/kunlabora/magnumsal/MagnumSal.kt
+++ src/main/kotlin/be/kunlabora/magnumsal/MagnumSal.kt
@@ -4,10 +4,17 @@ import be.kunlabora.magnumsal.MagnumSalEvent.PlayerAdded
 
 sealed class MagnumSalEvent: Event {
     data class PlayerAdded(val name: String) : MagnumSalEvent()
+    data class MinerPlaced(val by: String, val at: MineShaftPosition) : MagnumSalEvent()
 }
 
 class MagnumSal(private val eventStream: EventStream) {
     fun addPlayer(playerName: String) {
         eventStream.push(PlayerAdded(playerName))
     }
+
+    fun placeMiner(by: String, at: MineShaftPosition) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
+
+data class MineShaftPosition(private val _pos: Int)
--- src/test/kotlin/be/kunlabora/magnumsal/MagnumSalTest.kt
+++ src/test/kotlin/be/kunlabora/magnumsal/MagnumSalTest.kt
@@ -2,17 +2,32 @@ package be.kunlabora.magnumsal
 
 import be.kunlabora.magnumsal.MagnumSalEvent.*
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class MagnumSalTest {
 
+    private lateinit var eventStream: EventStream
+    private lateinit var magnumSal: MagnumSal
+
+    @BeforeEach
+    internal fun setUp() {
+        eventStream = EventStream()
+        magnumSal = MagnumSal(eventStream)
+    }
+
     @Test
     fun `A player can be added`() {
-        val eventStream = EventStream()
-        val magnumSal = MagnumSal(eventStream)
-
         magnumSal.addPlayer("Snarf")
 
         assertThat(eventStream).containsExactly(PlayerAdded("Snarf"))
+    }
+
+    @Test
+    fun `A Miner can be placed in the mineshaft's first position`() {
+        magnumSal.addPlayer("Snarf")
+        magnumSal.placeMiner("Snarf", MineShaftPosition(1))
+
+        assertThat(eventStream).containsExactly(MinerPlaced("Snarf", MineShaftPosition(1)))
     }
 }
