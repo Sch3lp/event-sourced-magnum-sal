51bafaf9f078fd2049b6035176a615e8d32244a2
sch3lp <sch3lp@gmail.com>
2019-12-12 13:33:47 +0100

    Adding a Miner at 2nd position in the shaft failing test

--- src/test/kotlin/be/kunlabora/magnumsal/MagnumSalTest.kt
+++ src/test/kotlin/be/kunlabora/magnumsal/MagnumSalTest.kt
@@ -5,6 +5,8 @@ import be.kunlabora.magnumsal.exception.IllegalMove
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class MagnumSalTest {
@@ -18,27 +20,57 @@ class MagnumSalTest {
         magnumSal = MagnumSal(eventStream)
     }
 
-    @Test
-    fun `A player can be added`() {
-        magnumSal.addPlayer("Snarf")
+    @Nested
+    inner class AddingPlayers {
+        @Test
+        fun `A player can be added`() {
+            magnumSal.addPlayer("Snarf")
 
-        assertThat(eventStream).containsExactly(PlayerAdded("Snarf"))
+            assertThat(eventStream).containsExactly(PlayerAdded("Snarf"))
+        }
     }
 
-    @Test
-    fun `A Miner cannot be placed by a player that's not in the game`() {
-        assertThatExceptionOfType(IllegalMove::class.java)
-                .isThrownBy { magnumSal.placeMiner("Snarf", MineShaftPosition(1)) }
-                .withMessage("Placing a miner requires Snarf to be a player in the game.")
-    }
 
-    @Test
-    fun `A Miner can be placed in the mineshaft's first position`() {
-        magnumSal.addPlayer("Snarf")
+    @Nested
+    inner class PlacingMiners {
+        @Test
+        fun `A Miner can be placed in the mineshaft's first position`() {
+            magnumSal.addPlayer("Snarf")
 
-        magnumSal.placeMiner("Snarf", MineShaftPosition(1))
+            magnumSal.placeMiner("Snarf", MineShaftPosition(1))
 
-        assertThat(eventStream)
-                .contains(MinerPlaced("Snarf", MineShaftPosition(1)))
+            assertThat(eventStream)
+                    .contains(MinerPlaced("Snarf", MineShaftPosition(1)))
+        }
+
+        @Test
+        fun `A Miner cannot be placed by a player that's not in the game`() {
+            assertThatExceptionOfType(IllegalMove::class.java)
+                    .isThrownBy { magnumSal.placeMiner("Snarf", MineShaftPosition(1)) }
+                    .withMessage("Placing a miner requires Snarf to be a player in the game.")
+
+            assertThat(eventStream)
+                    .doesNotContain(MinerPlaced("Snarf", MineShaftPosition(1)))
+        }
+
+        @Test
+        fun `A Miner cannot be placed in the mineshaft's second position without there being a miner in the first position`() {
+            magnumSal.addPlayer("Snarf")
+
+            assertThatExceptionOfType(IllegalMove::class.java)
+                    .isThrownBy { magnumSal.placeMiner("Snarf", MineShaftPosition(2)) }
+                    .withMessage("Placing a miner in the mineshaft at 2 requires there to be a miner in the mineshaft at 1.")
+
+            assertThat(eventStream)
+                    .doesNotContain(MinerPlaced("Snarf", MineShaftPosition(2)))
+        }
+
+        @Test
+        @Disabled("Test after removing miner")
+        fun `A Miner cannot be placed in the mineshaft's 7th position because there is no 7th position`() {
+            assertThatExceptionOfType(IllegalMove::class.java)
+                    .isThrownBy { magnumSal.placeMiner("Snarf", MineShaftPosition(7)) }
+                    .withMessage("Placing a miner in a non existing position is impossible")
+        }
     }
 }
