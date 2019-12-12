beb9bcda55e9e1fd713019ef29f8e9a266714340
sch3lp <sch3lp@gmail.com>
2019-12-12 12:58:41 +0100

    A player can be added

--- src/main/kotlin/be/kunlabora/magnumsal/MagnumSal.kt
+++ src/main/kotlin/be/kunlabora/magnumsal/MagnumSal.kt
@@ -1,11 +1,13 @@
 package be.kunlabora.magnumsal
 
+import be.kunlabora.magnumsal.MagnumSalEvent.PlayerAdded
+
 sealed class MagnumSalEvent: Event {
     data class PlayerAdded(val name: String) : MagnumSalEvent()
 }
 
 class MagnumSal(private val eventStream: EventStream) {
     fun addPlayer(playerName: String) {
-        TODO("add an event to the event stream")
+        eventStream.push(PlayerAdded(playerName))
     }
 }
