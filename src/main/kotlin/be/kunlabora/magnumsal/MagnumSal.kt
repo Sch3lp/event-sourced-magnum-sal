889631e841eb1e10d77e5bf19f86245e2367f5e4
sch3lp <sch3lp@gmail.com>
2019-12-12 15:17:56 +0100

    Fixed bad merge

--- src/main/kotlin/be/kunlabora/magnumsal/MagnumSal.kt
+++ src/main/kotlin/be/kunlabora/magnumsal/MagnumSal.kt
@@ -53,8 +53,4 @@ data class MineShaftPosition(private val _at: Int) {
     fun next(): MineShaftPosition = if (_at == 6) this else this.copy(_at = _at + 1)
     operator fun rangeTo(other: MineShaftPosition): List<MineShaftPosition> = (this._at..other._at).map { MineShaftPosition(it) }
     override fun toString(): String = "mineshaft[$_at]"
-
-    init {
-        if (_at !in 1..6) throw IllegalArgumentException("MineShaftPosition $_at does not exist.")
-    }
 }
